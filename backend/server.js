abcdd8ddae386a597c45af40d33b04faf97f2eb8
Mehboob451 <152257381+Mehboob451@users.noreply.github.com>
2026-08-23 10:34:02 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,5 +46,5 @@ app.get('/health', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(\Backend running on port \\);
+  console.log(`Backend running on port ${PORT}`);
 });
